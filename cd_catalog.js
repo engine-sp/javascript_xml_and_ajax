fadeeddbdd951e35cde1ca40b5408dedadad834b
engine <engine@longsun.net>
2023-08-17 16:12:55 +0800

    230817 upload

---
+++ cd_catalog.js
@@ -0,0 +1,106 @@
+[
+    {
+        "ARTIST": "Bob Dylan",
+        "TITLE": "Empire Burlesque"
+    },
+    {
+        "ARTIST": "Bonnie Tyler",
+        "TITLE": "Hide your heart"
+    },
+    {
+        "ARTIST": "Dolly Parton",
+        "TITLE": "Greatest Hits"
+    },
+    {
+        "ARTIST": "Gary Moore",
+        "TITLE": "Still got the blues"
+    },
+    {
+        "ARTIST": "Eros Ramazzotti",
+        "TITLE": "Eros"
+    },
+    {
+        "ARTIST": "Bee Gees",
+        "TITLE": "One night only"
+    },
+    {
+        "ARTIST": "Dr.Hook",
+        "TITLE": "Sylvias Mother"
+    },
+    {
+        "ARTIST": "Rod Stewart",
+        "TITLE": "Maggie May"
+    },
+    {
+        "ARTIST": "Andrea Bocelli",
+        "TITLE": "Romanza"
+    },
+    {
+        "ARTIST": "Percy Sledge",
+        "TITLE": "When a man loves a woman"
+    },
+    {
+        "ARTIST": "Savage Rose",
+        "TITLE": "Black angel"
+    },
+    {
+        "ARTIST": "Many",
+        "TITLE": "1999 Grammy Nominees"
+    },
+    {
+        "ARTIST": "Kenny Rogers",
+        "TITLE": "For the good times"
+    },
+    {
+        "ARTIST": "Will Smith",
+        "TITLE": "Big Willie style"
+    },
+    {
+        "ARTIST": "Van Morrison",
+        "TITLE": "Tupelo Honey"
+    },
+    {
+        "ARTIST": "Jorn Hoel",
+        "TITLE": "Soulsville"
+    },
+    {
+        "ARTIST": "Cat Stevens",
+        "TITLE": "The very best of"
+    },
+    {
+        "ARTIST": "Sam Brown",
+        "TITLE": "Stop"
+    },
+    {
+        "ARTIST": "T'Pau",
+        "TITLE": "Bridge of Spies"
+    },
+    {
+        "ARTIST": "Tina Turner",
+        "TITLE": "Private Dancer"
+    },
+    {
+        "ARTIST": "Kim Larsen",
+        "TITLE": "Midt om natten"
+    },
+    {
+        "ARTIST": "Luciano Pavarotti",
+        "TITLE": "Pavarotti Gala Concert"
+    },
+    {
+        "ARTIST": "Otis Redding",
+        "TITLE": "The dock of the bay"
+    },
+    {
+        "ARTIST": "Simply Red",
+        "TITLE": "Picture book"
+    },
+    {
+        "ARTIST": "The Communards",
+        "TITLE": "Red"
+    },
+    {
+        "ARTIST": "Joe Cocker",
+        "TITLE": "Unchain my heart"
+    }
+]
